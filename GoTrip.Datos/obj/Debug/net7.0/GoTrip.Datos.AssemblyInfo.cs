08b74d8185a2b9385f5cb868e54ec96deae19273
EmilseCampanelli <emicampanelli16@gmail.com>
2024-10-11 21:18:28 -0300

    Actualizar .gitignore para ignorar archivos de configuración y compilación

--- GoTrip.Datos/obj/Debug/net7.0/GoTrip.Datos.AssemblyInfo.cs
+++ GoTrip.Datos/obj/Debug/net7.0/GoTrip.Datos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoTrip.Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0d752de29959b82697ea5af6829a47416b3b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoTrip.Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoTrip.Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GoTrip.Dominio/obj/Debug/net7.0/GoTrip.Dominio.AssemblyInfo.cs
+++ GoTrip.Dominio/obj/Debug/net7.0/GoTrip.Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoTrip.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0d752de29959b82697ea5af6829a47416b3b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoTrip.Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoTrip.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

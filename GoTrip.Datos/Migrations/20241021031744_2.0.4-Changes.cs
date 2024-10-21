8f1989f549f06a023ed1bae6ea7fddfaa69337d4
EmilseCampanelli <emicampanelli16@gmail.com>
2024-10-21 00:18:46 -0300

    Update PathImage

---
+++ GoTrip.Datos/Migrations/20241021031744_2.0.4-Changes.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoTrip.Datos.Migrations
+{
+    /// <inheritdoc />
+    public partial class _204Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PathImagen",
+                table: "PuntosTuristicos",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PuntosTuristicos",
+                keyColumn: "PathImagen",
+                keyValue: null,
+                column: "PathImagen",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PathImagen",
+                table: "PuntosTuristicos",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
